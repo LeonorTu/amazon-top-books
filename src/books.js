c8aa4ba86ed2b784b987fa48ad6516e4d3c8a9a0
Leonor Tu <Jingyi.Tu@outlook.com>
2025-01-09 16:46:51 +0200

    feat: Add number to the top

---
+++ src/books.js
@@ -0,0 +1,24 @@
+// export const book = []
+const books = [
+  {
+    author: 'Mel Robbins',
+    title: 'The Let Them Theory',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL600_SR600,400_.jpg',
+    id: 1,
+  },
+  {
+    author: 'Eckhart Tolle',
+    title: 'A New Earth',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71IiqoxaNhL._AC_UL600_SR600,400_.jpg',
+    id: 2,
+  },
+  {
+    author: 'Martha Beck',
+    title: 'Beyond Anxiety',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81kfZ4lt4FL._AC_UL600_SR600,400_.jpg',
+    id: 3,
+  },
+]
+
+// or export default (only one default export per file)
+export default books
--- src/index.js
+++ src/index.js
@@ -1,45 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
-const books = [
-  {
-    author: 'Mel Robbins',
-    title: 'The Let Them Theory',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL600_SR600,400_.jpg',
-  },
-  {
-    author: 'Eckhart Tolle',
-    title: 'A New Earth',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/71IiqoxaNhL._AC_UL600_SR600,400_.jpg',
-  },
-  {
-    author: 'Martha Beck',
-    title: 'Beyond Anxiety',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81kfZ4lt4FL._AC_UL600_SR600,400_.jpg',
-  },
-]
+import books from './books'
+import Book from './Book'
 
 const BookList = () => {
   return (
     <section className='booklist'>
-      {books.map((book) => {
-        const { img, title, author } = book
-        return <Book img={img} title={title} author={author} />
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index} />
       })}
     </section>
   )
 }
 
-const Book = ({ img, title, author }) => {
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author} </h4>
-    </article>
-  )
-}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<BookList />) // need to close the bracket
